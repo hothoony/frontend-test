59030922d0ec2a4daefa4e2113c2f93e88459aef
hothoony <hothoony@hanmail.net>
2022-03-24 11:23:14 +0900

    .

--- javascript/es6_2015/Destructuring/destructuring_array.js
+++ javascript/es6_2015/Destructuring/destructuring_array.js
@@ -1,8 +1,33 @@
-let data = ['mac', 'linux', 'windows'];
+const colors = ['red', 'blue', 'green'];
+const fruits = ['apple', 'melon'];
 
-let osType1 = data[0];
-let osType2 = data[1];
-console.log(osType1, osType2);
+// extract value
+var c1 = colors[0];
+var c2 = colors[1];
+console.log();
+console.log(c1, c2);
 
-let [osType3,,osType4] = data;
-console.log(osType3, osType4);
+// destructuring
+var [c1, , c3] = colors;
+console.log();
+console.log(c1, c3);
+
+// default value
+var [c1, c2, c3, c4 = 'Orange'] = colors;
+console.log();
+console.log(c1, c2, c3, c4);
+
+var [c1, ...rest] = colors;
+console.log();
+console.log(c1);
+console.log(rest);
+
+// concat array
+var newArray = colors.concat(fruits);
+console.log();
+console.log(newArray);
+
+// concat array with destructuring
+var newArray = [...colors, ...fruits];
+console.log();
+console.log(newArray);
