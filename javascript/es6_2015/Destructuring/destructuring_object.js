271b881fe02ab5348a9da36e9d511ca390378484
hothoony <hothoony@hanmail.net>
2022-03-24 11:59:01 +0900

    .

--- javascript/es6_2015/Destructuring/destructuring_object.js
+++ javascript/es6_2015/Destructuring/destructuring_object.js
@@ -1,13 +1,44 @@
-let obj = {
+const person1 = {
+    name: 'james',
+    age: 20,
+    address: {
+        city: 'seoul',
+        street: 'mapo'
+    }
+}
+
+const person2 = {
     name: 'steve',
-    address: 'Korea',
-    age: 20
+    age: 22
 }
 
 // name, age 를 출력
-let {name, age} = obj;
+var { name, age } = person1;
+console.log();
 console.log(name, age);
 
 // 다른 이름으로 출력
-let {name: myName, age: myAge} = obj;
+var { name: myName, age: myAge } = person2;
+console.log();
 console.log(myName, myAge);
+
+// default value
+var { name, age, country: myCountry = 'korea' } = person2;
+console.log();
+console.log(name, age, myCountry);
+
+// object 합치기
+var newPerson = { ...person1, ...person2 };
+console.log();
+console.log(newPerson);
+
+// address 오브젝트를 destructuring
+var { address: { city, street } } = person1;
+console.log();
+console.log(city, street);
+
+// rest
+var { name, ...rest } = person1;
+console.log();
+console.log(name);
+console.log(rest);
