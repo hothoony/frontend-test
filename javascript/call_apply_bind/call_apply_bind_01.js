b6870288fe6c430fdd882efad5277a7c3913b5de
hothoony <hothoony@hanmail.net>
2022-12-24 13:22:05 +0900

    .

--- javascript/call_apply_bind/call_apply_bind_01.js
+++ javascript/call_apply_bind/call_apply_bind_01.js
@@ -1,8 +1,8 @@
-let result0 = Math.max(2, 1, 3);                 // 함수 기본 호출
-let result1 = Math.max.call(null, 2, 1, 3);      // call, 파라미터로 전달
-let result2 = Math.max.apply(null, [2, 1, 3]);   // apply, 배열로 전달
-let bindResult1 = Math.max.bind(null, 2, 1, 3);      // bind, 함수가 실행되지 않고 함수 자체를 리턴
-let bindResult2 = Math.max.bind(null);      // bind, 함수가 실행되지 않고 함수 자체를 리턴
+let result0 = Math.max(2, 1, 3);                // 함수 기본 호출
+let result1 = Math.max.call(null, 2, 1, 3);     // call, 파라미터로 전달
+let result2 = Math.max.apply(null, [2, 1, 3]);  // apply, 배열로 전달
+let bindResult1 = Math.max.bind(null, 2, 1, 3); // bind, 함수가 실행되지 않고 함수 자체를 리턴
+let bindResult2 = Math.max.bind(null);          // bind, 함수가 실행되지 않고 함수 자체를 리턴
 
 console.log('basic result  :', result0);
 console.log(' call result  :', result1);
