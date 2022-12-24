363c730003307ff0a8f8b226ad48e4537488d3d9
hothoony <hothoony@hanmail.net>
2022-12-24 13:18:08 +0900

    .

--- javascript/anonymous_function.js
+++ javascript/anonymous_function.js
@@ -1,5 +1,10 @@
-let result = (function() {
-    console.log('anonymous function called');
+let func1 = (function() {
+    console.log('anonymous function func1 called');
 })();
 
-console.log('result', result);
+let func2 = (function() {
+    console.log('anonymous function func2 called');
+}());
+
+console.log('func1', func1);
+console.log('func2', func2);
