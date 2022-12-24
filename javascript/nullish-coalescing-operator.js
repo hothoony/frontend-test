2306540d555958b3e6103abed5dcde14b2f40cb2
hothoony <hothoony@hanmail.net>
2022-12-24 17:27:01 +0900

    .

---
+++ javascript/nullish-coalescing-operator.js
@@ -0,0 +1,8 @@
+let data1 = null;
+let data2 = 'ok';
+
+let value1 = data1 ?? 'default value';
+console.log('value1 =', value1);
+
+let value2 = data2 ?? 'default value';
+console.log('value2 =', value2);
