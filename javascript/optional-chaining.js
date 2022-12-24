78fbb512a3834b9243247b66055e20c63ac9e389
hothoony <hothoony@hanmail.net>
2022-12-24 17:19:02 +0900

    .

---
+++ javascript/optional-chaining.js
@@ -0,0 +1,25 @@
+// obj?.prop
+// obj?.[prop]
+// obj.method?.()
+
+let user1 = {
+    name: 'john',
+    hello: () => `hello there?`,
+};
+
+let user2 = {};
+
+console.log('');
+console.log('# obj?.prop');
+console.log('user1 name =', user1?.name);
+console.log('user1 name =', user2?.name);
+
+console.log('');
+console.log('# obj?.[prop]');
+console.log('user1 name =', user1?.['name']);
+console.log('user1 name =', user2?.['name']);
+
+console.log('');
+console.log('# obj.method?.()');
+console.log('user1 name =', user1.hello?.());
+console.log('user1 name =', user2.hello?.());
