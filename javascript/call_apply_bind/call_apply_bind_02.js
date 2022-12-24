3c99725d15f54e55bca48cd7545ba4bcd1ce312e
hothoony <hothoony@hanmail.net>
2022-12-24 13:25:29 +0900

    .

--- javascript/call_apply_bind/call_apply_bind_02.js
+++ javascript/call_apply_bind/call_apply_bind_02.js
@@ -1,19 +1,19 @@
-var obj1 = {
+let obj1 = {
     name: 'objA',
     sum: function(a, b, c) {
         return 'hello ' + this.name + ' ' + (a + b + c);
     }
 }
 
-var obj2 = {
+let obj2 = {
     name: 'objB',
 }
 
-let result0 = obj1.sum(2, 1, 3);                 // 함수 기본 호출
-let result1 = obj1.sum.call(obj2, 2, 1, 3);      // call, 파라미터로 전달
-let result2 = obj1.sum.apply(obj2, [2, 1, 3]);   // apply, 배열로 전달
-let bindResult1 = obj1.sum.bind(obj2, 2, 1, 3);      // bind, 함수가 실행되지 않고 함수 자체를 리턴
-let bindResult2 = obj1.sum.bind(obj2);      // bind, 함수가 실행되지 않고 함수 자체를 리턴
+let result0 = obj1.sum(2, 1, 3);                // 함수 기본 호출
+let result1 = obj1.sum.call(obj2, 2, 1, 3);     // call, 파라미터로 전달
+let result2 = obj1.sum.apply(obj2, [2, 1, 3]);  // apply, 배열로 전달
+let bindResult1 = obj1.sum.bind(obj2, 2, 1, 3); // bind, 함수가 실행되지 않고 함수 자체를 리턴
+let bindResult2 = obj1.sum.bind(obj2);          // bind, 함수가 실행되지 않고 함수 자체를 리턴
 
 console.log('basic result  :', result0);
 console.log(' call result  :', result1);
