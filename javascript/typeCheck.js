0d23b6d60e4ce54adcae70b3d0fe13cb4c3997b4
hothoony <hothoony@hanmail.net>
2022-12-24 10:45:55 +0900

    .

--- javascript/typeCheck.js
+++ javascript/typeCheck.js
@@ -1,18 +1,28 @@
 (function() {
     const data = [
-        null,
-        undefined,
-        '',
-        ' ',
-        'abc',
-        '123',
-        456,
-        {},
-        [],
-        new String(),
-        new Number(),
-        new Date(),
-        arguments,
+        null,                   // [object Null]
+        undefined,              // [object Undefined]
+        '',                     // [object String]
+        ' ',                    // [object String]
+        'abc',                  // [object String]
+        '123',                  // [object String]
+        456,                    // [object Number]
+        {},                     // [object Object]
+        [],                     // [object Array]
+        new String(),           // [object String]
+        new Number(),           // [object Number]
+        new Date(),             // [object Date]
+        new Boolean(),          // [object Boolean]
+        arguments,              // [object Arguments]
+        NaN,                    // [object Number]
+        true,                   // [object Boolean]
+        false,                  // [object Boolean]
+        new Object(),           // [object Object]
+        (() => {}),             // [object Function]
+        (function() {}),        // [object Function]
+        (function() {}()),      // [object Undefined]
+        (function() {})(),      // [object Undefined]
+        new Error(),            // [object Error]
     ];
 
     for (let i of data) {
