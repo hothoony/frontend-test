2e77947f79b8282ea276847a24aacaaf22253d2c
hothoony <hothoony@hanmail.net>
2023-01-05 21:42:17 +0900

    .

---
+++ javascript/template-literals,template-strings.js
@@ -0,0 +1,11 @@
+// Template Literals
+// backtick(`) 을 사용한다
+
+let str;
+
+str = `hello world`;
+console.log('str =', str);
+
+let name = 'james';
+str = `hello ${name}?`;
+console.log('str =', str);
