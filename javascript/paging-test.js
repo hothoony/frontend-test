7474bf4870fea17caad8595af153ed730eef3baa
hothoony <hothoony@hanmail.net>
2023-02-10 18:09:58 +0900

    .

--- javascript/paging-test.js
+++ javascript/paging-test.js
@@ -10,9 +10,35 @@ let listSize = 10;
 let pageSize = 10;
 
 console.log('totalList', totalList);
+console.log('listSize', listSize);
+console.log('pageSize', pageSize);
 
 let currPage;
-let totalPage = Math.floor(totalList / listSize);
+let restPage = (totalList % listSize > 0) ? 1 : 0;
+let totalPage = Math.floor(totalList / listSize) + restPage;
+let pageGroup;
 
+console.log('restPage', restPage);
 console.log('totalPage', totalPage);
 
+/*
+1 => 1 2 3 4 5
+2 => 6 7 8 9 10
+3 => 11 12 13 14 15
+*/
+function printPage(page) {
+    let startPage = pageSize * (page - 1) + 1;
+    let endPage = startPage + pageSize;
+
+    
+
+    let result = [];
+    for (let i = startPage; i < endPage; i++) {
+        result.push(i);
+    }
+    console.log('page', page, '=>', result);
+}
+
+for (let i = 1; i <= 20; i++) {
+    printPage(i);
+}
