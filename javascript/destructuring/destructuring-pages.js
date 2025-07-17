54654a4488154b4912e2d5d8459011bc047fd3df
hothoony <hothoony@hanmail.net>
2025-07-17 10:19:26 +0900

    .

---
+++ javascript/destructuring/destructuring-pages.js
@@ -0,0 +1,26 @@
+const pages = {
+    "segments": {
+        "segment": [
+            {"id": 1, "insertTime": -10, "duration": 10},
+            {"id": 2, "insertTime": 0, "duration": 20},
+            {"id": 3, "insertTime": 20, "duration": 30},
+        ],
+        "background": [
+            {"id": 1, "insertTime": -10, "duration": 10},
+            {"id": 2, "insertTime": 0, "duration": 20},
+            {"id": 3, "insertTime": 20, "duration": 30},
+        ],
+        "image": [
+            {"id": 1, "insertTime": -10, "duration": 10},
+            {"id": 2, "insertTime": 0, "duration": 20},
+            {"id": 3, "insertTime": 20, "duration": 30},
+        ],
+        "title": [
+            {"id": 1, "insertTime": -10, "duration": 10},
+            {"id": 2, "insertTime": 0, "duration": 20},
+            {"id": 3, "insertTime": 20, "duration": 30},
+        ],
+    },
+};
+
+console.log('pages', pages);
