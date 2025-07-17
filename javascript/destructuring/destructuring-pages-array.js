9bbdc5ad79d24791358b0221bf7a11b74ed57efb
hothoony <hothoony@hanmail.net>
2025-07-17 11:19:42 +0900

    .

--- javascript/destructuring/destructuring-pages-array.js
+++ javascript/destructuring/destructuring-pages-array.js
@@ -1,19 +1,19 @@
 let pages = [{
     "segments": [{
         "segment": [
-            {"id": 1, "insertTime": -10, "duration": 10},
-            {"id": 2, "insertTime": 0, "duration": 20},
-            {"id": 3, "insertTime": 20, "duration": 30},
+            { "id": 1, "insertTime": -10, "duration": 10 },
+            { "id": 2, "insertTime": 0, "duration": 20 },
+            { "id": 3, "insertTime": 20, "duration": 30 },
         ],
         "background": [
-            {"id": 1, "insertTime": -10, "duration": 10},
-            {"id": 2, "insertTime": 0, "duration": 20},
-            {"id": 3, "insertTime": 20, "duration": 30},
+            { "id": 1, "insertTime": -10, "duration": 10 },
+            { "id": 2, "insertTime": 0, "duration": 20 },
+            { "id": 3, "insertTime": 20, "duration": 30 },
         ],
         "image": [
-            {"id": 1, "insertTime": -10, "duration": 10},
-            {"id": 2, "insertTime": 0, "duration": 20},
-            {"id": 3, "insertTime": 20, "duration": 30},
+            { "id": 1, "insertTime": -10, "duration": 10 },
+            { "id": 2, "insertTime": 0, "duration": 20 },
+            { "id": 3, "insertTime": 20, "duration": 30 },
         ],
     }],
 }];
@@ -53,9 +53,11 @@ console.log('result2=', result2);
 
 
 // background.insertTime 이 음수가 있으면 alert
-const existMinus = pages.some(page => ({
-
-}));
+const existMinus = pages.some(page =>
+    page.segments.some(segment =>
+        segment.background.some(background => background.insertTime < 0)
+    )
+);
 
 console.log();
 console.log('existMinus=', existMinus);
--- javascript/destructuring/destructuring-pages-object.js
+++ javascript/destructuring/destructuring-pages-object.js
@@ -1,19 +1,19 @@
 let pages = {
     "segments": {
         "segment": [
-            {"id": 1, "insertTime": -10, "duration": 10},
-            {"id": 2, "insertTime": 0, "duration": 20},
-            {"id": 3, "insertTime": 20, "duration": 30},
+            { "id": 1, "insertTime": -10, "duration": 10 },
+            { "id": 2, "insertTime": 0, "duration": 20 },
+            { "id": 3, "insertTime": 20, "duration": 30 },
         ],
         "background": [
-            {"id": 1, "insertTime": -10, "duration": 10},
-            {"id": 2, "insertTime": 0, "duration": 20},
-            {"id": 3, "insertTime": 20, "duration": 30},
+            { "id": 1, "insertTime": -10, "duration": 10 },
+            { "id": 2, "insertTime": 0, "duration": 20 },
+            { "id": 3, "insertTime": 20, "duration": 30 },
         ],
         "image": [
-            {"id": 1, "insertTime": -10, "duration": 10},
-            {"id": 2, "insertTime": 0, "duration": 20},
-            {"id": 3, "insertTime": 20, "duration": 30},
+            { "id": 1, "insertTime": -10, "duration": 10 },
+            { "id": 2, "insertTime": 0, "duration": 20 },
+            { "id": 3, "insertTime": 20, "duration": 30 },
         ],
     },
 };
