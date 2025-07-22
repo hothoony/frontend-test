57059f2d0f21d512c1ed7378fde047d1f78e20b1
hothoony <hothoony@hanmail.net>
2025-07-22 15:32:41 +0900

    .

--- emoji-test/emoji-test.js
+++ emoji-test/emoji-test.js
@@ -3,7 +3,12 @@ function remove4ByteChars(str) {
   return str.replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, '');
 }
 
-const original = "안녕하세요 😃😂😍😢😎🐱‍👤❤️💯🚀🌟";
+const emoji_3bytes = "©☀☕☎♥"; // 3바이트
+const emoji_4bytes = "😀😂😎🐶🚀"; // 4바이트
+
+let original = "안녕하세요 😃😂😍😢😎🐱‍👤❤️💯🚀🌟 abc 123";
+original += ", 3bytes: " + emoji_3bytes;
+original += ", 4bytes: " + emoji_4bytes;
 const filtered = remove4ByteChars(original);
 
 console.log("원본:", original);
